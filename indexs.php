7ba579fbecc775000ebaa94ac54d1697d411ec79
DonovanGSC <126727119+DonovanSaavedra@users.noreply.github.com>
2023-03-26 00:43:57 -0600

    Add files via upload
    Los php

---
+++ indexs.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <title>Practica 1 | Includes con PHP</title>
+</head>
+<body>
+    <!-- Navbar --> 
+  <?php include "include/navbar.php"; ?>
+    <!-- Termina navbar --> 
+    <?php include "include/menu.php"; ?>
+    <?php include "include/sumas.php"; ?>
+<script scr="js/bootstrap.min.js"></script>
+
+</body>
+</html>
