32de57dfefbbc59a1ff3410ee349fe0c71fb8c23
DonovanGSC <126727119+DonovanSaavedra@users.noreply.github.com>
2023-03-26 00:41:17 -0600

    include
    carpeta auto contenedora de los include

---
+++ include/Conexion.php
@@ -0,0 +1,13 @@
+<?php
+$servidor = "localhost";
+$Usuario = "root";
+$Pass = "";
+$BD = "practica";
+$conexion = mysqli_connect($servidor,$Usuario,$Pass,$BD);
+if($conexion->connect_error){
+die('error al conectar la base de datos,$coneccion->connect_error');
+}
+else{
+    echo"error alconectar BD";
+}
+?>
